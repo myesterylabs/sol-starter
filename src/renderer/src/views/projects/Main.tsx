8a60548444887316ae55c95553a7ddb95955cc90
Johnpaul <polymenjohn1@gmail.com>
2024-02-01 01:45:20 +0100

    redo projects UI

--- src/renderer/src/views/projects/Main.tsx
+++ src/renderer/src/views/projects/Main.tsx
@@ -1,4 +1,4 @@
-import * as $_ from "lodash";
+import * as $_ from 'lodash'
 
 import {
   Dropdown,
@@ -9,143 +9,63 @@ import {
   Lucide,
   Modal,
   ModalBody,
-  Tippy,
-} from "@/base-components";
+  Tippy
+} from '@/base-components'
 
-import { faker as $f } from "@/utils";
-import classnames from "classnames";
-import { useState } from "react";
+import { faker as $f } from '@/utils'
+import classnames from 'classnames'
+import { useState } from 'react'
 
 function Main() {
-  const [deleteConfirmationModal, setDeleteConfirmationModal] = useState(false);
+  const [deleteConfirmationModal, setDeleteConfirmationModal] = useState(false)
 
   return (
     <>
-      <h2 className="intro-y text-lg font-medium mt-10">Data List Layout</h2>
+      <h2 className="intro-y text-lg font-medium mt-10">Projects</h2>
       <div className="grid grid-cols-12 gap-6 mt-5">
         <div className="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
-          <button className="btn btn-primary shadow-md mr-2">
-            Add New Product
-          </button>
-          <Dropdown>
-            <DropdownToggle className="btn px-2 box">
-              <span className="w-5 h-5 flex items-center justify-center">
-                <Lucide icon="Plus" className="w-4 h-4" />
-              </span>
-            </DropdownToggle>
-            <DropdownMenu className="w-40">
-              <DropdownContent>
-                <DropdownItem>
-                  <Lucide icon="Printer" className="w-4 h-4 mr-2" /> Print
-                </DropdownItem>
-                <DropdownItem>
-                  <Lucide icon="FileText" className="w-4 h-4 mr-2" /> Export to
-                  Excel
-                </DropdownItem>
-                <DropdownItem>
-                  <Lucide icon="FileText" className="w-4 h-4 mr-2" /> Export to
-                  PDF
-                </DropdownItem>
-              </DropdownContent>
-            </DropdownMenu>
-          </Dropdown>
-          <div className="hidden md:block mx-auto text-slate-500">
-            Showing 1 to 10 of 150 entries
-          </div>
-          <div className="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
-            <div className="w-56 relative text-slate-500">
-              <input
-                type="text"
-                className="form-control w-56 box pr-10"
-                placeholder="Search..."
-              />
-              <Lucide
-                icon="Search"
-                className="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0"
-              />
-            </div>
-          </div>
+          <button className="btn btn-primary shadow-md mr-2">Create Project</button>
         </div>
         {/* BEGIN: Data List */}
         <div className="intro-y col-span-12 overflow-auto lg:overflow-visible">
-          <table className="table table-report -mt-2">
+          <table className="table table-report sm:mt-2">
             <thead>
               <tr>
-                <th className="whitespace-nowrap">IMAGES</th>
-                <th className="whitespace-nowrap">PRODUCT NAME</th>
-                <th className="text-center whitespace-nowrap">STOCK</th>
+                {/* <th className="whitespace-nowrap">IMAGES</th> */}
+                <th className="whitespace-nowrap">PROJECT NAME</th>
+                <th className="text-center whitespace-nowrap">LAST MODIFIED</th>
                 <th className="text-center whitespace-nowrap">STATUS</th>
                 <th className="text-center whitespace-nowrap">ACTIONS</th>
               </tr>
             </thead>
             <tbody>
-              {$_.take($f(), 9).map((faker, fakerKey) => (
+              {$_.take($f(), 4).map((faker, fakerKey) => (
                 <tr key={fakerKey} className="intro-x">
-                  <td className="w-40">
-                    <div className="flex">
-                      <div className="w-10 h-10 image-fit zoom-in">
-                        <Tippy
-                          tag="img"
-                          alt="Midone Tailwind HTML Admin Template"
-                          className="rounded-full"
-                          src={faker.images[0]}
-                          content={`Uploaded at ${faker.dates[0]}`}
-                        />
-                      </div>
-                      <div className="w-10 h-10 image-fit zoom-in -ml-5">
-                        <Tippy
-                          tag="img"
-                          alt="Midone Tailwind HTML Admin Template"
-                          className="rounded-full"
-                          src={faker.images[1]}
-                          content={`Uploaded at ${faker.dates[1]}`}
-                        />
-                      </div>
-                      <div className="w-10 h-10 image-fit zoom-in -ml-5">
-                        <Tippy
-                          tag="img"
-                          alt="Midone Tailwind HTML Admin Template"
-                          className="rounded-full"
-                          src={faker.images[2]}
-                          content={`Uploaded at ${faker.dates[2]}`}
-                        />
-                      </div>
-                    </div>
-                  </td>
                   <td>
                     <a href="" className="font-medium whitespace-nowrap">
                       {faker.products[0].name}
                     </a>
-                    <div className="text-slate-500 text-xs whitespace-nowrap mt-0.5">
-                      {faker.products[0].category}
-                    </div>
+                    <div className="text-slate-500 text-xs whitespace-nowrap mt-0.5">V 0.0.1</div>
                   </td>
-                  <td className="text-center">{faker.stocks[0]}</td>
+                  <td className="text-center">26 Jan 2024</td>
                   <td className="w-40">
                     <div
                       className={classnames({
-                        "flex items-center justify-center": true,
-                        "text-success": faker.trueFalse[0],
-                        "text-danger": !faker.trueFalse[0],
+                        'flex items-center justify-center': true,
+                        'text-success': faker.trueFalse[0],
+                        'text-danger': !faker.trueFalse[0]
                       })}
                     >
-                      <Lucide icon="CheckSquare" className="w-4 h-4 mr-2" />
-                      {faker.trueFalse[0] ? "Active" : "Inactive"}
+                      {faker.trueFalse[0] ? 'Deployed' : 'Local'}
                     </div>
                   </td>
                   <td className="table-report__action w-56">
                     <div className="flex justify-center items-center">
-                      <a className="flex items-center mr-3" href="#">
-                        <Lucide icon="CheckSquare" className="w-4 h-4 mr-1" />{" "}
-                        Edit
+                      <a className="flex items-center mr-3" href="">
+                        <Lucide icon="Code" className="w-4 h-4 mr-1" />
+                        Code
                       </a>
-                      <a
-                        className="flex items-center text-danger"
-                        href="#"
-                        onClick={() => {
-                          setDeleteConfirmationModal(true);
-                        }}
-                      >
+                      <a className="flex items-center text-danger" href="">
                         <Lucide icon="Trash2" className="w-4 h-4 mr-1" /> Delete
                       </a>
                     </div>
@@ -156,79 +76,17 @@ function Main() {
           </table>
         </div>
         {/* END: Data List */}
-        {/* BEGIN: Pagination */}
-        <div className="intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-nowrap items-center">
-          <nav className="w-full sm:w-auto sm:mr-auto">
-            <ul className="pagination">
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  <Lucide icon="ChevronsLeft" className="w-4 h-4" />
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  <Lucide icon="ChevronLeft" className="w-4 h-4" />
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  ...
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  1
-                </a>
-              </li>
-              <li className="page-item active">
-                <a className="page-link" href="#">
-                  2
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  3
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  ...
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  <Lucide icon="ChevronRight" className="w-4 h-4" />
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  <Lucide icon="ChevronsRight" className="w-4 h-4" />
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <select className="w-20 form-select box mt-3 sm:mt-0">
-            <option>10</option>
-            <option>25</option>
-            <option>35</option>
-            <option>50</option>
-          </select>
-        </div>
-        {/* END: Pagination */}
       </div>
       {/* BEGIN: Delete Confirmation Modal */}
       <Modal
         show={deleteConfirmationModal}
         onHidden={() => {
-          setDeleteConfirmationModal(false);
+          setDeleteConfirmationModal(false)
         }}
       >
         <ModalBody className="p-0">
           <div className="p-5 text-center">
-            <Lucide
-              icon="XCircle"
-              className="w-16 h-16 text-danger mx-auto mt-3"
-            />
+            <Lucide icon="XCircle" className="w-16 h-16 text-danger mx-auto mt-3" />
             <div className="text-3xl mt-5">Are you sure?</div>
             <div className="text-slate-500 mt-2">
               Do you really want to delete these records? <br />
@@ -239,7 +97,7 @@ function Main() {
             <button
               type="button"
               onClick={() => {
-                setDeleteConfirmationModal(false);
+                setDeleteConfirmationModal(false)
               }}
               className="btn btn-outline-secondary w-24 mr-1"
             >
@@ -253,7 +111,7 @@ function Main() {
       </Modal>
       {/* END: Delete Confirmation Modal */}
     </>
-  );
+  )
 }
 
-export default Main;
+export default Main
